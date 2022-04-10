56ceeec2b239f93de948dd903d5a2ebfddf73c82
Pedro A. González <pgonzalez@post.com>
2022-04-09 19:01:19 -0500

    Implement head function

---
+++ head.js
@@ -0,0 +1,14 @@
+// REQUIREMENTS
+// const assertEqual = require('./assertEqual');
+
+// FUNCTION IMPLEMENTATION
+const head = array => array[0];
+
+// EXPORT FUNCTION
+module.exports = head;
+
+// TEST CODE
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(['hello', 'more', 'things']), 'hello');
+// assertEqual(head([1]), 1);
+// assertEqual(head([]), undefined);
