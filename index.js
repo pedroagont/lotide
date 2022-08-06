2f3f94ebd00e50c23f57437cf533b5b4c1d6f0e8
Pedro A. González <pgonzalez@post.com>
2022-08-06 00:39:03 -0500

    Refactor files in js directory

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const head = require('./head');
-const isPalindrome = require('./isPalindrome');
-const letterPositions = require('./letterPositions');
-const middle = require('./middle');
-const tail = require('./tail');
-const without = require('./without');
+const assertArraysEqual = require('./js/assertArraysEqual');
+const assertEqual = require('./js/assertEqual');
+const assertObjectsEqual = require('./js/assertObjectsEqual');
+const countLetters = require('./js/countLetters');
+const countOnly = require('./js/countOnly');
+const eqArrays = require('./js/eqArrays');
+const eqObjects = require('./js/eqObjects');
+const findKeyByValue = require('./js/findKeyByValue');
+const flatten = require('./js/flatten');
+const head = require('./js/head');
+const isPalindrome = require('./js/isPalindrome');
+const letterPositions = require('./js/letterPositions');
+const middle = require('./js/middle');
+const tail = require('./js/tail');
+const without = require('./js/without');
 
 module.exports = {
   assertArraysEqual,
