64ea38d581aac96b3578242d0f3ac534ce2bfee0
Pedro A. González <pgonzalez@post.com>
2022-08-06 00:36:31 -0500

    Create index to require and export all functions

---
+++ index.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const isPalindrome = require('./isPalindrome');
+const letterPositions = require('./letterPositions');
+const middle = require('./middle');
+const tail = require('./tail');
+const without = require('./without');
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  flatten,
+  head,
+  isPalindrome,
+  letterPositions,
+  middle,
+  tail,
+  without
+};
