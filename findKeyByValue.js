0975137fb96f4cad773d19f14df31a2413a842da
Pedro A. González <pgonzalez@post.com>
2022-08-05 23:51:19 -0500

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+// REQUIREMENTS
+const assertEqual = require('./assertEqual');
+
+// FUNCTION IMPLEMENTATION
+const findKeyByValue = (object, value) => {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+// EXPORT FUNCTION
+module.exports = findKeyByValue;
+
+// TEST CASES
+const bestTVShowsByGenre = {
+  sci_fi: 'The Expanse',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire'
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
